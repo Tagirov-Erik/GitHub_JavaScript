989ba358601bf33f7aaeec860cf5e9f27b8ed642
Tagirov-Erik <eriktagirov@gmail.com>
2019-06-30 15:56:18 -0400

    Remove one 'else' level

--- HW_1_Lesson26_06/js/app.js
+++ HW_1_Lesson26_06/js/app.js
@@ -10,20 +10,22 @@ function getSignAndType(num) {
 
     if (isNaN(num)) {
         console.log("Not a number.");
-    } else {
-        if (num < 0) {
-            signNum = "negative";
-        } else if (num > 0) {
-            signNum = "positive";
-        } else if (num == 0) {
-            signNum = "zero";
-        }
-
-        if (intNum == true) {
-            typeNum = "integer";
-        } else {
-            typeNum = "float";
-        }
+        return;
     }
+    
+    if (num < 0) {
+        signNum = "negative";
+    } else if (num > 0) {
+        signNum = "positive";
+    } else if (num == 0) {
+        signNum = "zero";
+    }
+
+    if (intNum == true) {
+        typeNum = "integer";
+    } else {
+        typeNum = "float";
+    }
+
     console.log("Number " + num + " is " + typeNum + " and " + signNum + ".")
 }
