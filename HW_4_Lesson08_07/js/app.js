5b5954b4cef80423e29de9d11eb5e91de5024b2b
Tagirov-Erik <eriktagirov@gmail.com>
2019-07-10 05:36:59 -0400

    Some code upgrade

--- HW_4_Lesson08_07/js/app.js
+++ HW_4_Lesson08_07/js/app.js
@@ -4,12 +4,32 @@ $('#confirmBtn').click(checkConfirm);
  * Check validation input. Validation logic: every text field must be > 5 symbols.
  */
 function checkConfirm() {
+    $('input').next().removeClass('error');
     var firstName = $('#firstName').val();
     var lastName = $('#lastName').val();
     var userEmail = $('#userEmail').val();
-   
-    if (firstName.length >= 5 && lastName.length >= 5 && userEmail.length >= 5) {
-        $('input').next().removeClass('error');
+
+    if (firstName.length < 5) {
+        $('#firstName').val('');
+        $('#firstName').next().addClass('error');
+    } else {
+        var passFirstName = true;
+    }
+    if (lastName.length < 5) {
+        $('#lastName').val('');
+        $('#lastName').next().addClass('error');
+    } else {
+        var passLastName = true;
+    }
+    if (userEmail.length < 5) {
+        $('#userEmail').val('');
+        $('#userEmail').next().addClass('error');
+    } else {
+        var passEmail = true;
+    }
+
+    if (passFirstName && passLastName && passEmail) {
+        $('input').next().removeClass('error'); //clear errors after pass
         var userInfo = {};
         userInfo.firstName = firstName;
         userInfo.lastName = lastName;
@@ -17,24 +37,6 @@ function checkConfirm() {
         alert("Succes. Object created.");
         console.log(userInfo);
         return;
-    }
-    if (firstName.length < 5) {
-        $('#firstName').val('');
-        $('#firstName').next().addClass('error');
-    } else {
-        $('#firstName').next().removeClass('error');
-    }
-    if (lastName.length < 5) {
-        $('#lastName').val('');
-        $('#lastName').next().addClass('error');
-    } else {
-        $('#lastName').next().removeClass('error');
-    }
-    if (userEmail.length < 5) {
-        $('#userEmail').val('');
-        $('#userEmail').next().addClass('error');
-    } else {
-        $('#userEmail').next().removeClass('error');
     }
 }
 
